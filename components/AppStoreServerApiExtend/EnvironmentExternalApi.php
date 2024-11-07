7ff607b3db92180b1bd397bdd533fb1f833c4b34
raul.ionut.pop <raul.ionut.pop@reea.net>
2024-11-07 11:19:08 +0200

    Initial commit

---
+++ components/AppStoreServerApiExtend/EnvironmentExternalApi.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace app\components\AppStoreServerApiExtend;
+
+final class EnvironmentExternalApi
+{
+    /**
+     * @var string
+     */
+    const PRODUCTION_EXTERNAL = 'Production_external_api';
+
+    /**
+     * @var string
+     */
+    const SANDBOX_EXTERNAL = 'Sandbox_external_api';
+}
